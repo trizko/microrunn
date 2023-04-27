3da45dca4ce9512bd061309571c6ffeb9518fd25
Tony Rizko <tony@rizkocircle.com>
2023-04-26 23:13:57 -0700

    Update v2::Value to use RefCell for data and grad

--- src/engine/v2.rs
+++ src/engine/v2.rs
@@ -1,10 +1,10 @@
-use std::rc::Rc;
 use std::cell::RefCell;
 use std::fmt::Debug;
+use std::rc::Rc;
 
 pub struct Value {
-    pub data: f64,
-    pub grad: f64,
+    pub data: RefCell<f64>,
+    pub grad: RefCell<f64>,
     pub _prev: Vec<Rc<RefCell<Value>>>,
     _op: Op,
 }
@@ -21,19 +21,34 @@ enum Op {
 impl Value {
     pub fn new(data: f64) -> Value {
         Value {
-            data,
-            grad: 0.0,
+            data: RefCell::new(data),
+            grad: RefCell::new(0.0),
             _prev: vec![],
             _op: Op::None,
         }
     }
 
     pub fn tanh(self) -> Value {
-        let data: f64 = self.data.tanh();
-        let grad: f64 = 0.0;
-        let left = Rc::new(RefCell::new(self));
+        let data: RefCell<f64> = RefCell::new(self.data.borrow().tanh());
+        let grad: RefCell<f64> = RefCell::new(0.0);
+        let left: Rc<RefCell<Value>> = Rc::new(RefCell::new(self));
         let _prev: Vec<Rc<RefCell<Value>>> = vec![left];
         let _op: Op = Op::Tanh;
+
+        Value {
+            data,
+            grad,
+            _prev,
+            _op,
+        }
+    }
+
+    pub fn powf(self, n: f64) -> Value {
+        let data: RefCell<f64> = RefCell::new(self.data.borrow().powf(n));
+        let grad: RefCell<f64> = RefCell::new(0.0);
+        let left = Rc::new(RefCell::new(self));
+        let _prev: Vec<Rc<RefCell<Value>>> = vec![left];
+        let _op: Op = Op::Powf(n);
 
         Value {
             data,
