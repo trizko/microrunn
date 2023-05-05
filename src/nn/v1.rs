527d16eeee4f44cf66a4bc8edfd0a4504e83fc35
Tony Rizko <tony@rizkocircle.com>
2023-05-05 12:05:47 -0700

    Add tests for Neuron type

--- src/nn/v1.rs
+++ src/nn/v1.rs
@@ -74,10 +74,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_neuron() {
+    fn create_neuron_adds_correct_amount_of_weights() {
         let n = Neuron::new(6, true);
 
         assert_eq!(6, n.weights.len());
+    }
+    #[test]
+    fn create_neuron_adds_random_value_weights() {
+        let n = Neuron::new(6, true);
+
+        n.weights.iter().for_each(|v| {
+            assert_ne!(0.0, *v.borrow().data.borrow());
+        });
+    }
+    #[test]
+    fn create_neuron_adds_random_value_bias() {
+        let n = Neuron::new(6, true);
+
+        assert_ne!(0.0, *n.bias.borrow().data.borrow());
     }
     #[test]
     fn create_output_from_neuron() {
@@ -97,7 +111,6 @@ mod tests {
         let n = Neuron::new(3, true);
         let out = n.call(x);
 
-        assert_eq!(3, n.weights.len());
         assert_eq!(0.0, *out.grad.borrow());
     }
     #[test]
@@ -119,7 +132,6 @@ mod tests {
         let out = n.call(x);
         out.backward();
 
-        assert_eq!(3, n.weights.len());
-        assert_eq!(0.0, *out.grad.borrow());
+        assert_ne!(0.0, *out.grad.borrow());
     }
 }
